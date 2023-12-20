6da3dbdf0be0d8db648c501b996f2bc42a98d02d
ggdoe <gregoire.doebele@ens-paris-saclay.fr>
2023-12-20 23:43:17 +0100

    fixing edge

--- pcolor.h
+++ pcolor.h
@@ -10,7 +10,7 @@ struct pcolor_state{
     int ld_pixels; // configurable
 
     uint32_t *pixels;
-    int *pixel_color_id;
+    uint32_t *pixel_color_id;
     enum pixel_type {
         PCOLOR_TYPE_CELL,
         PCOLOR_TYPE_EDGE,
@@ -21,21 +21,6 @@ struct pcolor_state{
     uint32_t color_outside;
     bool show_edge;
 };
-
-// void draw_cross(uint32_t *pixels, int w_pixels, int h_pixels, int ld_pixels, int i, int j, uint32_t color)
-// {
-//     const int w_cross = 5;
-
-//     int imin = (i - w_cross     < 0)        ?        0 : i - w_cross;
-//     int imax = (i + w_cross + 1 > h_pixels) ? h_pixels : i + w_cross + 1;
-//     int jmin = (j - w_cross     < 0)        ?        0 : j - w_cross;
-//     int jmax = (j + w_cross + 1 > w_pixels) ? w_pixels : j + w_cross + 1;
-
-//     for(int jj = jmin; jj < jmax; jj++)
-//         pixels[i * ld_pixels + jj] = color;
-//     for(int ii = imin; ii < imax; ii++)
-//         pixels[ii * ld_pixels + j] = color;
-// }
 
 void draw_line(uint32_t *pixels, int w_pixels, int h_pixels, int i0, int j0, int i1, int j1, uint32_t color)
 {
@@ -119,7 +104,7 @@ struct pcolor_state pcolor_state_alloc(uint32_t *pixels, int w_pixels, int h_pix
         .color_outside = 0xFF111111,
         .show_edge = true
     };
-    state.pixel_color_id = realloc(state.pixel_color_id, w_pixels*h_pixels*sizeof(int));
+    state.pixel_color_id = realloc(state.pixel_color_id, w_pixels*h_pixels*sizeof(uint32_t));
     state.pixel_type     = realloc(state.pixel_type    , w_pixels*h_pixels*sizeof(enum pixel_type));
     return state;
 }
@@ -136,10 +121,8 @@ void pcolor_fill_state(struct pcolor_state *state, double *x, double *y, int w_g
     const int w_pixels  = state->w_pixels;
     const int h_pixels  = state->h_pixels;
 
-    for (int i = 0; i < w_pixels * h_pixels; i++){
-        state->pixel_color_id[i] = -1;
+    for (int i = 0; i < w_pixels * h_pixels; i++)
         state->pixel_type[i] = PCOLOR_TYPE_OUTSIDE;
-    }
 
     const double xmin = _pcolor_min(x, w_grid*h_grid), ymin = _pcolor_min(y, w_grid*h_grid);
     const double xmax = _pcolor_max(x, w_grid*h_grid), ymax = _pcolor_max(y, w_grid*h_grid);
@@ -167,17 +150,21 @@ void pcolor_fill_state(struct pcolor_state *state, double *x, double *y, int w_g
             int imin = _pcolor_min_int(iv, 4);
             int imax = _pcolor_max_int(iv, 4);
 
+
             for(int id = imin; id <= imax; id++)
                 for(int jd = jmin; jd <= jmax; jd++)
                     if(is_inside_quad(iv, jv, id, jd)){
                         state->pixel_type[id * w_pixels + jd] = PCOLOR_TYPE_CELL;
                         state->pixel_color_id[id * w_pixels + jd] = i * (w_grid-1) + j;
                     }
-                    
+            draw_line(state->pixel_color_id, w_pixels, h_pixels, iv[0], jv[0], iv[1], jv[1], i * (w_grid-1) + j);
+            draw_line(state->pixel_color_id, w_pixels, h_pixels, iv[0], jv[0], iv[3], jv[3], i * (w_grid-1) + j);
+            
             draw_line(state->pixel_type, w_pixels, h_pixels, iv[0], jv[0], iv[1], jv[1], PCOLOR_TYPE_EDGE);
             draw_line(state->pixel_type, w_pixels, h_pixels, iv[0], jv[0], iv[3], jv[3], PCOLOR_TYPE_EDGE);
             draw_line(state->pixel_type, w_pixels, h_pixels, iv[3], jv[3], iv[2], jv[2], PCOLOR_TYPE_EDGE);
             draw_line(state->pixel_type, w_pixels, h_pixels, iv[1], jv[1], iv[2], jv[2], PCOLOR_TYPE_EDGE);
+
         }
     }
 }
@@ -194,9 +181,7 @@ void pcolor(struct pcolor_state *state, uint32_t *color_grid)
                     state->pixels[id_pixels] = color_grid[state->pixel_color_id[id_color]];
                     break;
                 case PCOLOR_TYPE_EDGE:
-                    state->pixels[id_pixels] = (state->show_edge) ? state->color_egde : 
-                                    (state->pixel_color_id[id_color] != -1) ? color_grid[state->pixel_color_id[id_color]] : 
-                                        state->color_outside;
+                    state->pixels[id_pixels] = (state->show_edge) ? state->color_egde : color_grid[state->pixel_color_id[id_color]];
                     break;
                 case PCOLOR_TYPE_OUTSIDE:
                     state->pixels[id_pixels] = state->color_outside;
@@ -237,8 +222,8 @@ void pcolor_nostate(uint32_t *pixels, int w_pixels, int h_pixels, double *x, dou
             int imin = _pcolor_min_int(iv, 4);
             int imax = _pcolor_max_int(iv, 4);
 
-            for(int id = imin; id < imax; id++)
-                for(int jd = jmin; jd < jmax; jd++)
+            for(int id = imin; id <= imax; id++)
+                for(int jd = jmin; jd <= jmax; jd++)
                     if(is_inside_quad(iv, jv, id, jd))
                         pixels[id * w_pixels + jd] =  color_grid[i * (w_grid-1) + j];
             
