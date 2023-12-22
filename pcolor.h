b219264740aa9c812e048f52641acfd3b3c75cf2
ggdoe <gregoire.doebele@ens-paris-saclay.fr>
2023-12-22 20:33:25 +0100

    leading dimension for pcolor

--- pcolor.h
+++ pcolor.h
@@ -65,6 +65,22 @@ bool is_inside_quad(int *iv, int *jv, int i, int j)
     return true;
 }
 
+static double _pcolor_min_ld(const double *x, int n, int m, int ld)
+{
+    double min = x[0];
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            min = (x[i*ld+j] < min) ? x[i*ld+j] : min;
+    return min;
+}
+static double _pcolor_max_ld(const double *x, int n, int m, int ld)
+{
+    double max = x[0];
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < m; j++)
+            max = (x[i*ld+j] > max) ? x[i*ld+j] : max;
+    return max;
+}
 static double _pcolor_min(const double *x, int n)
 {
     double min = x[0];
@@ -127,24 +143,24 @@ void pcolor_fill_state(struct pcolor_state *state, double *vertex_x, double *ver
         state->pixel_type[i] = PCOLOR_TYPE_OUTSIDE;
     }
 
-    const double xmin = _pcolor_min(vertex_x, (Nx+1)*(Ny+1)), ymin = _pcolor_min(vertex_y, (Nx+1)*(Ny+1));
-    const double xmax = _pcolor_max(vertex_x, (Nx+1)*(Ny+1)), ymax = _pcolor_max(vertex_y, (Nx+1)*(Ny+1));
+    const double xmin = _pcolor_min_ld(vertex_x, (Ny+1), (Nx+1), ld+1), ymin = _pcolor_min_ld(vertex_y, (Ny+1), (Nx+1), ld+1);
+    const double xmax = _pcolor_max_ld(vertex_x, (Ny+1), (Nx+1), ld+1), ymax = _pcolor_max_ld(vertex_y, (Ny+1), (Nx+1), ld+1);
     
     for (int i = 0; i < Ny; i++) {
         for (int j = 0; j < Nx; j++) {
 
             // vertex of the current cell
             int jv[] = {
-                (vertex_x[ i    * (Nx+1) + j  ] - xmin) / (xmax - xmin) * (w_pixels-1), 
-                (vertex_x[ i    * (Nx+1) + j+1] - xmin) / (xmax - xmin) * (w_pixels-1), 
-                (vertex_x[(i+1) * (Nx+1) + j+1] - xmin) / (xmax - xmin) * (w_pixels-1), 
-                (vertex_x[(i+1) * (Nx+1) + j  ] - xmin) / (xmax - xmin) * (w_pixels-1)
+                (vertex_x[ i    * (ld+1) + j  ] - xmin) / (xmax - xmin) * (w_pixels-1), 
+                (vertex_x[ i    * (ld+1) + j+1] - xmin) / (xmax - xmin) * (w_pixels-1), 
+                (vertex_x[(i+1) * (ld+1) + j+1] - xmin) / (xmax - xmin) * (w_pixels-1), 
+                (vertex_x[(i+1) * (ld+1) + j  ] - xmin) / (xmax - xmin) * (w_pixels-1)
             };
             int iv[] = {
-                (vertex_y[ i    * (Nx+1) + j  ] - ymin) / (ymax - ymin) * (h_pixels-1), 
-                (vertex_y[ i    * (Nx+1) + j+1] - ymin) / (ymax - ymin) * (h_pixels-1), 
-                (vertex_y[(i+1) * (Nx+1) + j+1] - ymin) / (ymax - ymin) * (h_pixels-1),
-                (vertex_y[(i+1) * (Nx+1) + j  ] - ymin) / (ymax - ymin) * (h_pixels-1)
+                (vertex_y[ i    * (ld+1) + j  ] - ymin) / (ymax - ymin) * (h_pixels-1), 
+                (vertex_y[ i    * (ld+1) + j+1] - ymin) / (ymax - ymin) * (h_pixels-1), 
+                (vertex_y[(i+1) * (ld+1) + j+1] - ymin) / (ymax - ymin) * (h_pixels-1),
+                (vertex_y[(i+1) * (ld+1) + j  ] - ymin) / (ymax - ymin) * (h_pixels-1)
             };
 
             // boundary of the box
@@ -157,7 +173,7 @@ void pcolor_fill_state(struct pcolor_state *state, double *vertex_x, double *ver
                 for(int jd = jmin; jd <= jmax; jd++)
                     if(is_inside_quad(iv, jv, id, jd)){
                         state->pixel_type    [id * w_pixels + jd] = PCOLOR_TYPE_CELL;
-                        state->pixel_color_id[id * w_pixels + jd] = i * Nx + j;
+                        state->pixel_color_id[id * w_pixels + jd] = i * ld + j;
                     }
 
             draw_line(state->pixel_type, w_pixels, h_pixels, iv[0], jv[0], iv[1], jv[1], PCOLOR_TYPE_EDGE);
@@ -191,6 +207,7 @@ void pcolor(struct pcolor_state *state, uint32_t *color_grid)
 
 void pcolor_real(struct pcolor_state *state, real_t *values, real_t min, real_t max)
 {
+    #pragma omp parallel for collapse(2)
     for(int i = 0; i < state->h_pixels; i++)
         for(int j = 0; j < state->w_pixels; j++)
         {
